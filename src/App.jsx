4f835e7c83c058a4b438fc22ddc98c326448a47a
Favour Vivia Woka <vivianemma97@gmail.com>
2024-08-30 16:13:45 +0100

    structure event

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import AuthRoute from "./routes/AuthRoute";
+import EventRoute from "./routes/EventRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/*" element={<AuthRoute />} />
       <Route path="/auth/*" element={<AuthRoute />} />
+      <Route path="/event/*" element={<EventRoute />} />
     </Routes>
   );
 }
