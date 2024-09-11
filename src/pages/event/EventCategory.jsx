31f7782717653d22a2caa56fd9315d60444dfea5
Favour Vivia Woka <vivianemma97@gmail.com>
2024-09-11 06:13:38 +0100

    Fix design bug

--- src/pages/event/EventCategory.jsx
+++ src/pages/event/EventCategory.jsx
@@ -4,35 +4,6 @@ import { useForm } from "react-hook-form";
 import cloudy from "../../assets/icon/cloud.png";
 
 function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
-  const Input = ({ label, name, register, required }) => (
-    <div>
-      <label htmlFor={name}>{label}</label>
-      <input id={name} {...register(name, { required })} />
-    </div>
-  );
-
-  // you can use React.forwardRef to pass the ref too
-  const Select = React.forwardRef(
-    ({ onChange, onBlur, name, label, options }, ref) => (
-      <div>
-        <label htmlFor={name}>{label}</label>
-        <select
-          id={name}
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur}
-        >
-          {options.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      </div>
-    )
-  );
-
   const {
     register,
     formState: { errors },
@@ -44,23 +15,42 @@ function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
     console.log(data);
     handleNextStep();
   };
+  // you can use React.forwardRef to pass the ref too
+  const Select = React.forwardRef(
+    ({ onChange, onBlur, name, options }, ref) => (
+      <select
+        id={name}
+        name={name}
+        ref={ref}
+        onChange={onChange}
+        onBlur={onBlur}>
+        <option value="">Select</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    )
+  );
 
   return (
     <div className="flex flex-col justify-between w-[100%] h-[100%]">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <div className="w-full text-center">
-            <h5>Add Category</h5>
-            <p>Upload event thumbnail</p>
-          </div>
-          <div className="w-full text-center mt-[32px] border-dashed border-2 py-5 px-6">
+        <div className="mb-6 mt-4 text-center">
+          <h5 className="mb-2">Create a new Event Category</h5>
+          <p className="text-[#8C94A6]">Upload event thumbnail</p>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="w-full text-center border-dashed border-2 py-5 px-6 mb-4">
             <button className="spinner">
-              <img src={cloudy} alt="" />{" "}
+              <img src={cloudy} alt="" />
             </button>
             <div className="mt-4">
               <span className="text-[#EB5017] font-semibold">
-                Click to upload{" "}
-              </span>{" "}
+                Click to upload
+              </span>
               <span className="text-[#98A2B3]"> or drag and drop </span> <br />
               <small className="text-[#98A2B3]">
                 SVG, PNG, JPG or GIF (max. 800x400px)
@@ -76,7 +66,8 @@ function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
               <button className="button_primary w-1/2">Browse Files</button>
             </div>
           </div>
-          <div className="mt-[18px]">
+
+          <div className="mb-4">
             <label htmlFor="Event Type">Event Type</label>
             <Select
               {...register("physicalEvent", { required: true })}
@@ -86,7 +77,8 @@ function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
               <span className="text-[red]">Event type is required</span>
             )}
           </div>
-          <div className="mt-[18px]">
+
+          <div className="mb-4">
             <label htmlFor="Event Location">Event Location</label>
             <input
               type="text"
@@ -98,10 +90,11 @@ function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
               <span className="text-[red]">Event location is required</span>
             )}
           </div>
-          <div className="mt-[18px]">
+
+          <div className="mb-4">
             <label htmlFor="Event Category"> Event Category</label>
             <Select
-              {...register("conference", { required: true })}
+              {...register("eventCategory", { required: true })}
               options={[
                 "Conference",
                 "Info Session",
@@ -115,32 +108,27 @@ function EventCategory({ step, handleNextStep, handlePrevStep, data }) {
               <span className="text-[red]">Event category is required</span>
             )}
           </div>
-          <div className="my-6">
-            <span className="text-[#667185] ">
-              You can set up a{" "}
-              <span className="text-[#8F2802]">
-                custom domain or connect your email service provider
-              </span>{" "}
-              to change this.
-            </span>
-          </div>
-          <hr />
         </div>
+        <div className="mb-4">
+          <span className="mr-2 text-[#98a2b3]">You can set up a</span>
+          <span className="mr-2  text-[#8F2802]">
+            custom domain or connect your email service provider
+          </span>
+          <span className="text-[#98a2b3]">to change this.</span>
+        </div>
+        <hr className="mb-4" />
         <div className="flex justify-between">
           <button
             type="button"
             onClick={handlePrevStep}
             disabled={step === 0}
-            className="button_primary_outline w-[36%]"
-          >
+            className="button_primary_outline w-[36%]">
             Previous
           </button>
           <button
             type="submit"
-            onClick={handleNextStep}
             disabled={step === data.length - 1}
-            className="button_primary w-[60%]"
-          >
+            className="button_primary w-[60%]">
             Next Step
           </button>
         </div>
--- src/pages/event/EventSocial.jsx
+++ src/pages/event/EventSocial.jsx
@@ -18,99 +18,95 @@ function EventSocial({ step, handleNextStep, handlePrevStep, data }) {
   return (
     <div className="flex flex-col justify-between w-[100%] h-[100%]">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <div className="mb-[24px]">
-            <div className="text-center mb-8">
-              <h5 className="mt-8">Event Social Details</h5>
-              <p className="text-[#8C94A6] mt-2">
-                Fill out these details to create your event
-              </p>
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="Event Website">Event Website</label>
-              <input
-                type="text"
-                {...register("eventWebsite", { required: true })}
-                placeholder="WWW."
-                aria-invalid={errors.eventWebsite ? "true" : "false"}
-              />
-              {errors.eventWebsite?.type === "required" && (
-                <span className="text-[red]">Event website is required</span>
-              )}
-            </div>
-            <div className="mb-4">
-              <label htmlFor="Event Facebook Link">Event Facebook Link</label>
-              <input
-                type="text"
-                {...register("eventFacebookLink", { required: true })}
-                placeholder="https://www.facebook.com/"
-                aria-invalid={errors.eventFacebookLink ? "true" : "false"}
-              />
-              {errors.eventFacebookLink?.type === "required" && (
-                <span className="text-[red]">Facebook link is required</span>
-              )}
-            </div>
-            <div className="mb-4">
-              <label htmlFor="Event Instagram Link">Event Instagram Link</label>
-              <input
-                type="text"
-                {...register("eventInstagramLink", { required: true })}
-                placeholder="https://www.instagram.com/"
-                aria-invalid={errors.eventInstagramLink ? "true" : "false"}
-              />
-              {errors.eventInstagramLink?.type === "required" && (
-                <span className="text-[red]">Instagram Link is required</span>
-              )}
-            </div>
-            <div className="mb-4">
-              <label htmlFor="Event X (Twitter) Link">
-                Event X (Twitter) Link
-              </label>
-              <input
-                type="text"
-                {...register("eventTwitterLink", { required: true })}
-                placeholder="https://x.com/"
-                aria-invalid={errors.eventTwitterLink ? "true" : "false"}
-              />
-              {errors.eventTwitterLink?.type === "required" && (
-                <span className="text-[red]">Twitter link is required</span>
-              )}
-            </div>
-          </div>
-
-          <hr />
-
-          <div className="flex justify-between mt-[16px]">
-            <p>Run only once per customer</p>
-            <img src={toggle} alt="" />
-          </div>
-
-          <div className="my-[32px]">
-            <span className="mr-2 text-[#98a2b3]">You can set up a</span>
-            <span className="mr-2  text-[#8F2802]">
-              custom domain or connect your email service provider
-            </span>
-            <span className="text-[#98a2b3]">to change this.</span>
-          </div>
-
-          <hr />
+        <div className="mb-6 mt-4 text-center">
+          <h5 className="mb-2">Event Social Details</h5>
+          <p className="text-[#8C94A6]">
+            Fill out these details to create your event
+          </p>
         </div>
-        <div className="flex justify-between mt-[24px]">
+
+        <div className="flex flex-col">
+          <div className="mb-4">
+            <label htmlFor="Event Website">Event Website</label>
+            <input
+              type="text"
+              {...register("eventWebsite", { required: true })}
+              placeholder="WWW."
+              aria-invalid={errors.eventWebsite ? "true" : "false"}
+            />
+            {errors.eventWebsite?.type === "required" && (
+              <span className="text-[red]">Event website is required</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="Event Facebook Link">Event Facebook Link</label>
+            <input
+              type="text"
+              {...register("eventFacebookLink", { required: true })}
+              placeholder="https://www.facebook.com/"
+              aria-invalid={errors.eventFacebookLink ? "true" : "false"}
+            />
+            {errors.eventFacebookLink?.type === "required" && (
+              <span className="text-[red]">Facebook link is required</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="Event Instagram Link">Event Instagram Link</label>
+            <input
+              type="text"
+              {...register("eventInstagramLink", { required: true })}
+              placeholder="https://www.instagram.com/"
+              aria-invalid={errors.eventInstagramLink ? "true" : "false"}
+            />
+            {errors.eventInstagramLink?.type === "required" && (
+              <span className="text-[red]">Instagram Link is required</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="Event X (Twitter) Link">
+              Event X (Twitter) Link
+            </label>
+            <input
+              type="text"
+              {...register("eventTwitterLink", { required: true })}
+              placeholder="https://x.com/"
+              aria-invalid={errors.eventTwitterLink ? "true" : "false"}
+            />
+            {errors.eventTwitterLink?.type === "required" && (
+              <span className="text-[red]">Twitter link is required</span>
+            )}
+          </div>
+        </div>
+
+        <hr className="mb-4" />
+
+        <div className="flex justify-between mb-4">
+          <span>Run only once per customer</span>
+          <img src={toggle} alt="" />
+        </div>
+
+        <div className="mb-4">
+          <span className="mr-2 text-[#98a2b3]">You can set up a</span>
+          <span className="mr-2  text-[#8F2802]">
+            custom domain or connect your email service provider
+          </span>
+          <span className="text-[#98a2b3]">to change this.</span>
+        </div>
+
+        <hr className="mb-4" />
+
+        <div className="flex justify-between">
           <button
             type="button"
             onClick={handlePrevStep}
             disabled={step === 0}
-            className="button_primary_outline w-[36%]"
-          >
+            className="button_primary_outline w-[36%]">
             Previous
           </button>
           <button
             type="submit"
-            onClick={handleNextStep}
             disabled={step === data.length - 1}
-            className="button_primary w-[60%]"
-          >
+            className="button_primary w-[60%]">
             Next Step
           </button>
         </div>
